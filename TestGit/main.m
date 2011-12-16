bcbcd6415fb045f1700b2c10793624eb1e80b854
Steve Knodl <sjknodl@yahoo.com>
2011-12-15 21:04:25 -0600

    Initial Commit

---
+++ TestGit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestGit
+//
+//  Created by Steven Knodl on 12/15/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TGAppDelegate class]));
+    }
+}
