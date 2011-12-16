0c1b97792bee17468e1af35d6dab9d6a7f4a7778
Steve Knodl <sjknodl@yahoo.com>
2011-12-15 21:49:35 -0600

    Second Commit

--- TestGit/TGAppDelegate.h
+++ TestGit/TGAppDelegate.h
@@ -3,7 +3,7 @@
 //  TestGit
 //
 //  Created by Steven Knodl on 12/15/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 TheTestCompany. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
